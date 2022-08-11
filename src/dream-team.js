398ffee08b4ca2fa8b5d39bda00328c99d0093ee
Syderi <syderi@yahoo.com>
2022-08-11 13:32:46 +0300

    feat dreem teem

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,54 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+
+//  let aa = ['Matt', 'Ann', 'Dmitry', 'Max'];
+
+// let aa = [
+//   '   William Alston ',
+//   ' Paul Benacerraf',
+//   '  Ross Cameron',
+//   '       Gilles Deleuze',
+//   '  Arda Denkel ',
+//   '  Michael Devitt',
+//   '  Kit Fine',
+//   ' Nelson Goodman',
+//   'David Kolb',
+//   '   Saul Kripke',
+//   '  Trenton Merricks',
+//   '  Jay Rosenberg',
+// ]
+// let aa = [
+//   ['David Abram'],
+//   ['Robin Attfield'],
+//   'Thomas Berry',
+//   ['Paul R.Ehrlich'],
+//   'donna Haraway',
+//   ' BrIaN_gOodWiN  ',
+//   {
+//     0: 'Serenella Iovino'
+//   },
+//   'Erazim Kohak',
+//   '  val_plumwood',
+// ]
+// , 'ADGJKMNPRSTW');  'BDETV'
+
+
+//  createDreamTeam(aa)
+
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (!Array.isArray(members)) return false;
+  let result = '';
+  let newArray = members.filter(item => typeof item === 'string').map((tech) => tech.replaceAll(" ", "").toUpperCase()).sort();
+  console.log('newArray',newArray);
+  newArray.forEach(element => {
+    result = result + element.slice(0,1);
+
+    console.log('result',result);
+  });
+ return result;
 }
 
 module.exports = {
