aea53d2b1ef1a472dca457755b97de8609a1bf0c
Syderi <syderi@yahoo.com>
2022-10-10 12:18:10 +0300

    fix:all

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,63 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+// class VigenereCipheringMachine {
+
+//   constructor()
+
+//   shirf
+
+  
+//   encrypt() {
+//     throw new NotImplementedError('Not implemented');
+//     // remove line with error and write your code here
+//   }
+//   decrypt() {
+//     return this.encDec(1, text, key);
+//   }
+// }
+
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor(state = true) {
+    this.state = state;
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+ 
+  encrypt(text, key) {
+    return this.cipher(text, key, -1);
+  }
+
+  decrypt(text, key) {
+    return this.cipher(text, key, 1);
+  }
+
+  cipher(text, key, direction) {
+
+    if (!key || !text) {
+      throw new Error("Incorrect arguments!");
+    } 
+
+    key = key.toUpperCase();
+    text = text.toUpperCase();
+    let numberLetterKey = 0;
+    
+          const cipherArray = [...text].map((letter) => {         
+                  let letterPositionCode = letter.charCodeAt(0);
+
+                  if (letterPositionCode >= 65 && letterPositionCode < 91) {
+                      let curentKey = numberLetterKey % key.length;
+                      let keyPositionCode = direction * (key.charCodeAt(curentKey) - 130);
+                      numberLetterKey ++;
+                      let letterCharCodeCipher = (letterPositionCode - keyPositionCode) % 26;
+                  return String.fromCharCode(letterCharCodeCipher + 65);
+                  }
+
+            return letter;
+          });
+    if (this.state) {
+      return cipherArray.join("");
+    }
+    return cipherArray.reverse().join("");
   }
 }
 
